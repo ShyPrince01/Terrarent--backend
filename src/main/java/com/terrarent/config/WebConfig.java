576204627d8dd8aa095bd0512bf2aa7279ec4a56
ShyPrince01 <fait>
2026-07-02 20:38:27 +0100

    updated corss

--- src/main/java/com/terrarent/config/WebConfig.java
+++ src/main/java/com/terrarent/config/WebConfig.java
@@ -8,7 +8,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "http://localhost:3000", 
+                    "http://localhost:3000", "https://terrarent.vercel.app"
+                    
                 ) 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
